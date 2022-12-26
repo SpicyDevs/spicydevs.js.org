1fd6f23c6b6204ef7a1ab192636b36f9d4bf8e70
Aayu5h <aayushmagrawal@gmail.com>
2022-12-26 12:33:58 +0530

    Add files via upload

---
+++ portfolio01/js/custom.js
@@ -0,0 +1,9 @@
+/*!
+ * Item: Kitzu
+ * Description: Personal Portfolio Template
+ * Author/Developer: Exill
+ * Author/Developer URL: https://themeforest.net/user/exill
+ * Version: v1.1.0
+ * License: Themeforest Standard Licenses: https://themeforest.net/licenses
+ */
+!function(n){"use strict";n((function(){})),n(window).on("load",(function(){}))}(jQuery);
